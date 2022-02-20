e2cc6535853dd7f72a2bf38fa7791cf80df4492e
BrownRiceStudio <christopher.hyunmin.lee@gmail.com>
2022-02-20 21:21:31 +0900

    Player Controller

---
+++ Battle Card Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private Vector2 acceleration;
+
+    public float speed;
+    public float accelFactor;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        Vector2 targetVelocity = speed * movement.normalized;
+
+        acceleration = (targetVelocity - rb.velocity) * (accelFactor / (2 * speed));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = rb.velocity + acceleration;
+    }
+}
